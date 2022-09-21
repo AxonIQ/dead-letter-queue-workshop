0357cba757843770bf99fd9f21f447abca13ba62
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2022-09-21 08:57:04 +0200

    Solution of step 2

--- src/main/java/io/axoniq/workshop/dlq/config/WorkshopConfiguration.java
+++ src/main/java/io/axoniq/workshop/dlq/config/WorkshopConfiguration.java
@@ -1,6 +1,7 @@
 package io.axoniq.workshop.dlq.config;
 
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.PropagatingErrorHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ public class WorkshopConfiguration {
 
     @Autowired
     public void configureEventProcessing(EventProcessingConfigurer configurer) {
-        // TODO: Configure event processing here
+        configurer.registerListenerInvocationErrorHandler("product_name",
+                                                          configuration -> PropagatingErrorHandler.instance());
     }
 }
