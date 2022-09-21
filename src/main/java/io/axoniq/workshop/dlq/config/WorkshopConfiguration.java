763a5bd86991616cbb63d964aafb908ffad66268
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2022-09-21 08:58:03 +0200

    Solution of step 3

--- src/main/java/io/axoniq/workshop/dlq/config/WorkshopConfiguration.java
+++ src/main/java/io/axoniq/workshop/dlq/config/WorkshopConfiguration.java
@@ -1,7 +1,9 @@
 package io.axoniq.workshop.dlq.config;
 
+import org.axonframework.common.jpa.EntityManagerProvider;
+import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.EventProcessingConfigurer;
-import org.axonframework.eventhandling.PropagatingErrorHandler;
+import org.axonframework.eventhandling.deadletter.jpa.JpaSequencedDeadLetterQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,15 @@ public class WorkshopConfiguration {
 
     @Autowired
     public void configureEventProcessing(EventProcessingConfigurer configurer) {
-        configurer.registerListenerInvocationErrorHandler("product_name",
-                                                          configuration -> PropagatingErrorHandler.instance());
+        configurer.registerDeadLetterQueue("product_name",
+                                           configuration -> JpaSequencedDeadLetterQueue
+                                                   .builder()
+                                                   .processingGroup("product_name")
+                                                   .maxSequences(256)
+                                                   .maxSequenceSize(256)
+                                                   .entityManagerProvider(configuration.getComponent(EntityManagerProvider.class))
+                                                   .transactionManager(configuration.getComponent(TransactionManager.class))
+                                                   .serializer(configuration.serializer())
+                                                   .build());
     }
 }
