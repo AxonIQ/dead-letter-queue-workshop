f003eced1f7c0c3413d6bf771a0f8bd976e0cf0a
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2022-09-21 09:06:26 +0200

    Solution of step 5

--- src/main/java/io/axoniq/workshop/dlq/config/WorkshopConfiguration.java
+++ src/main/java/io/axoniq/workshop/dlq/config/WorkshopConfiguration.java
@@ -4,6 +4,7 @@ import org.axonframework.common.jpa.EntityManagerProvider;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventhandling.deadletter.jpa.JpaSequencedDeadLetterQueue;
+import org.axonframework.messaging.deadletter.Decisions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,5 +23,13 @@ public class WorkshopConfiguration {
                                                    .transactionManager(configuration.getComponent(TransactionManager.class))
                                                    .serializer(configuration.serializer())
                                                    .build());
+
+        configurer.registerDeadLetterPolicy("product_name", configuration -> (letter, cause) -> {
+            Integer retries = (Integer) letter.diagnostics().getOrDefault("retries", 0);
+            if(retries < 5) {
+                return Decisions.requeue(cause, l -> l.diagnostics().and("retries", retries + 1));
+            }
+            return Decisions.doNotEnqueue();
+        });
     }
 }
