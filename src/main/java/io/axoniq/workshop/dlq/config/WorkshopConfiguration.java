77328a6f626cfa1ba57d40fbcf3a6ffa933c5cac
Steven van Beelen <steven.vanbeelen@axoniq.io>
2024-09-10 16:19:17 +0200

    Indentation
    Indentation
    
    #enhancement/axoniq-con-24

--- src/main/java/io/axoniq/workshop/dlq/config/WorkshopConfiguration.java
+++ src/main/java/io/axoniq/workshop/dlq/config/WorkshopConfiguration.java
@@ -4,6 +4,7 @@ import org.axonframework.common.jpa.EntityManagerProvider;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventhandling.deadletter.jpa.JpaSequencedDeadLetterQueue;
+import org.axonframework.messaging.deadletter.Decisions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,10 +31,9 @@ public class WorkshopConfiguration {
 
         configurer.registerDeadLetterPolicy("product_name", configuration -> (letter, cause) -> {
             Integer retries = (Integer) letter.diagnostics().getOrDefault("retries", 0);
-            if(retries < 5) {
-                return Decisions.requeue(cause, l -> l.diagnostics().and("retries", retries + 1));
-            }
-            return Decisions.doNotEnqueue();
+            return retries < 5
+                    ? Decisions.requeue(cause, l -> l.diagnostics().and("retries", retries + 1))
+                    : Decisions.doNotEnqueue();
         });
     }
 }
